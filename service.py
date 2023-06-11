157f8da2013b87d534d3092d7c9937bd5c7cc1b3
Tony Edgin <tedgin@cyverse.org>
2023-06-11 08:49:57 -0700

    Documented service policy

--- service.py
+++ service.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-"""CyVerse-wide service account policy."""
+"""CyVerse-wide service account policy.
+
+A service account has a user type of 'ds-service'. It has no home or trash
+collection, and it is not a member of the public group.
+"""
 
 import irods_errors  # type: ignore
 import session_vars  # type: ignore
@@ -8,15 +12,11 @@ import session_vars  # type: ignore
 import irods_extra
 
 
-_SVC_TYPE = 'service'
+_SVC_TYPE = 'ds-service'
 
 
 def acCreateUser(_, cb, rei):
-    """Create a service type account.
-
-    A service type account doens't have a home or trash collection, and it
-    doesn't belong to the public group.
-    """
+    """Create a service type account."""
     user_type = session_vars.get_map(rei).get('other_user').get('user_type')
 
     if user_type == _SVC_TYPE:
